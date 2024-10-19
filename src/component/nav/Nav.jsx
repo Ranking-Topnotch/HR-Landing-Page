7bf97bd2d66d3a0bdc1adf6ec6937772238b3d68
Luke Emmanuel .C <124929220+Ranking-Topnotch@users.noreply.github.com>
2024-10-19 17:09:24 +0100

    second commit nav bar

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoClose } from "react-icons/io5";
+import Image from '../../image/687pIkbEsTthg.png'
 import './nav.css';
-import Image1 from '../../image/687pIkbEsTthg.png'
-
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-      <img className="logo" src={Image1} alt='Shamzbridge Consult' height={50} width={50}/> 
-      <ul className="nav-links">
-        <li><a href="#about-us">About Us</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
+      <img src={Image} alt='Shamzbridge Cousult' height={50} width={50} />
+      
+      {/* Hamburger icon / Close icon for mobile screens */}
+      <div className="hamburger" onClick={toggleMenu}>
+        {isOpen ? <IoClose size={30} color="white" /> : <GiHamburgerMenu size={30} color="white" />}
+      </div>
+
+      {/* Navbar links that toggle visibility on smaller screens */}
+      <ul className={isOpen ? "nav-links open" : "nav-links"}>
+        <li><a href="#about-us" onClick={toggleMenu}>About Us</a></li>
+        <li><a href="#services" onClick={toggleMenu}>Services</a></li>
+        <li><a href="#contact" onClick={toggleMenu}>Contact</a></li>
       </ul>
     </nav>
   );
